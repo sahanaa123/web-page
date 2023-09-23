80a7fa03fdadda612de0eb787fcba17f79cf789e
sahanaa123 <144119306+sahanaa123@users.noreply.github.com>
2023-09-23 14:07:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const hourEI = document.getElementById("hour");
+const minuteEI = document.getElementById("minute");
+const secondsEI = document.getElementById("seconds");
+const ampmeI = document.getElementById("ampm");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function updateclock(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+    if(h>12) {
+        h=h-12
+        ampm="PM"
+     }
+     h=h<10 ? "0"+"h":h;
+     m=m<10 ? "0"+"m":m;
+     s=s<10 ? "0"+"s":s;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+     hourEI.innerText=h;
+     minuteEI.innerText=m;
+     secondsEI.innerText=s;
+     ampmEI,(innerText=ampm);
+     setTimeout(()=>{
+        updateclock();
+
+     },1000)
+    }
+    updateclock();
